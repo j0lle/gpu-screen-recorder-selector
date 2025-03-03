902556b143342b8e03474fc62ab363c41c6d30a3
dec05eba <dec05eba@protonmail.com>
2025-03-03 13:17:35 +0100

    Pulseaudio audio device capture: dont change default output when switching default output

--- src/sound.cpp
+++ src/sound.cpp
@@ -234,10 +234,11 @@ static pa_handle* pa_sound_device_new(const char *server,
     if(!startup_get_default_devices(p, device_name))
         goto fail;
 
-    pa_context_set_subscribe_callback(p->context, subscribe_cb, p);
-    pa = pa_context_subscribe(p->context, PA_SUBSCRIPTION_MASK_SERVER, NULL, NULL);
-    if(pa)
-        pa_operation_unref(pa);
+    // TODO: This has to be disabled because of bugs in pipewire (and kde plasma?)
+    //pa_context_set_subscribe_callback(p->context, subscribe_cb, p);
+    //pa = pa_context_subscribe(p->context, PA_SUBSCRIPTION_MASK_SERVER, NULL, NULL);
+    //if(pa)
+    //    pa_operation_unref(pa);
 
     return p;
 
