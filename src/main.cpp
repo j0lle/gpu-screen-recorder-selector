3c5514480c93f498dfb29b513bef51fb858e4101
dec05eba <dec05eba@protonmail.com>
2025-09-29 10:07:08 +0200

    Remove use of libavcodec/defs.h (fix compile on old ffmpeg)

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,6 @@ extern "C" {
 extern "C" {
 #include <libavutil/pixfmt.h>
 #include <libavcodec/avcodec.h>
-#include <libavcodec/defs.h>
 #include <libavformat/avformat.h>
 #include <libavutil/opt.h>
 #include <libswresample/swresample.h>
